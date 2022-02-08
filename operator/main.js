35c54547ccee025db7161e0dced87e522e0b944b
Baek Han <han.baek.program@gmail.com>
2022-02-07 19:28:52 -0800

    Add operator case part 2

--- operator/main.js
+++ operator/main.js
@@ -80,3 +80,122 @@ console.log('-----------');
 
 
 ///// 7. Equality /////
+const stringFive = '5';
+const numberFive = 5;
+//  == loose equality, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+//  === strict equality, no type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+// object equality by reference
+const han1 = {name: 'han'};
+const han2 = {name: 'han'};
+const han3 = han1;
+console.log(han1 == han2);  // false
+console.log(han1 === han2); // false
+console.log(han1 === han3); // true
+// equality ex
+console.log(0 == false);          // true
+console.log(0 === false);         // false
+console.log('' == false);         // true
+console.log('' === false);        // false
+console.log(null == undefined);   // true
+console.log(null === undefined);  // false
+console.log('-----------');
+
+
+///// 8. Conditional Operators  /////
+// if, else if, else
+const name = 'coder';
+if (name === 'han') {
+  console.log('Welcome, Han!');
+} else if (name === 'coder') {
+  console.log('You are amazing coder');
+} else {
+  console.log('unknown');
+}
+console.log('-----------');
+
+
+///// 9. Ternary Operator: ?  /////
+// condition ? value1(true) : value2(false)
+console.log(name === 'han' ? 'yes' : 'no');
+console.log('-----------');
+
+
+///// 10. Switch Statement  /////
+//  use for multiple if checks
+//  use for enum-like value checks
+//  use for multiple type checks in TS
+const browser = 'IE';
+switch (browser) {
+  case 'IE':      // if (browser == 'IE')
+    console.log('go away!!');
+    break;
+  case 'Chrome':
+  case 'Firefox': // if (browser == 'Chrome' || browser == 'Firefox')
+    console.log('love you!');
+    break;
+  default:        // else
+    console.log('same all!');
+    break;
+}
+console.log('-----------');
+
+
+///// 11. Loops(while loop)  /////
+//  while the condition is truthy,
+//  body code is executed
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+console.log('-----------');
+
+
+///// 12. Loops(do while loop)  /////
+//  body code is executed first,
+//  then check the condition
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+console.log('-----------');
+
+
+///// 13. Loops(for loop)  /////
+//  for (begin; condition; step)
+for (i = 3; i > 0; i++) {
+  console.log(`for: ${i}`);
+}
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i: ${i}, j: ${j}`);
+  }
+}
+console.log('-----------');
+
+
+///// 14. Break  /////
+//  Stop loop
+for (let i = 0; i < 10; i++) {
+  if (i == 8) {
+    break;
+  }
+  console.log(`for: ${i}`);
+}
+console.log('-----------');
+
+
+///// 15. Continue  /////
+//  Skip loop
+for (let i = 0; i < 10; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(`for: ${i}`);
+}
+console.log('-----------');
