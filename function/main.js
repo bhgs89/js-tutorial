cac6d743b434c23794219b5e0af8ec881e66939d
Baek Han <han.baek.program@gmail.com>
2022-02-08 23:32:59 -0800

    Add function case part 2

---
+++ function/main.js
@@ -0,0 +1,108 @@
+'use strict';
+
+///// Function /////
+//  - fundamental building block in the program
+//  - subprogram can be used multiple times
+//  - performs a task or calculates a value
+
+///// 1. Function declaration /////
+// function name(param1, param2) { body ... return; }
+// one function === one thing
+// naming: doSomthing, command, verb
+// e.g createCardAndPoint -> createCard, createPoint
+// function is object in JS
+function printHello() {
+  console.log('Hello');
+}
+printHello();
+
+function log(message) {
+  console.log(message);
+}
+log('Hello@');
+log(1234);
+console.log('---------------');
+
+
+///// 2. Parameters /////
+//  premitive parameters: passed by value
+//  object parameters: passed by reference
+function changeName(obj) {
+  obj.name = 'coder';
+}
+const han = { name: 'han' };
+changeName(han);
+console.log(han);
+console.log('---------------');
+
+
+///// 3. Default parameters (added in ES6) /////
+function showMessage(message, from = 'unknown') {
+  console.log(`${message} by ${from}`);
+}
+showMessgae('Hi!');
+console.log('---------------');
+
+
+///// 4. Rest parameters (added in ES6) /////
+//  - Parameters을 배열 형태로 전달 받는다
+function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+
+  for (const arg of args) {
+    console.log(arg);
+  }
+
+  args.forEach(e => {
+    console.log(e);
+  });
+}
+printfAll('dream', 'coding', 'han');
+console.log('---------------');
+
+
+///// 5. Local scope /////
+//  - 밖에서는 안이 보이지 않고, 안에서만 밖을 볼 수 있다
+let globalMessage = 'global'; // global variable
+function printMessage() {
+  let message = 'hello';
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let childMessage = 'hello2';
+  }
+  // console.log(childMessage);  // error
+}
+printMessage();
+// console.log(message); // error
+console.log('---------------');
+
+
+///// 6. Return a value /////
+//  - return이 없는 function은 return undefined; 와 같다
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+console.log('---------------');
+
+
+///// 7. early return, early exit /////
+// bad case
+function upgradeBadUser(user) {
+  if (user.point > 10) {
+    // long upgrade logic...
+  }
+}
+// good case
+function upgradeGoodUser(user) {
+  if (user.point <= 10) {
+    return;
+  }
+  // long upgrade logic...
+}
+console.log('---------------');
---
+++ function/main2.js
@@ -0,0 +1,72 @@
+'use strict';
+
+///// First-class function /////
+//  function are treated like any other variable
+//  can be assigned as a value to variable
+//  can be passed as an argument to other functions
+//  can be returned by another function
+
+///// 1. Function expression /////
+//  a function declaration can be called earlier than it is defined. (hoisted)
+//  a function expression is created when the execution reaches it.
+const print = function() {  // anonymous function
+  console.log('print');
+};
+const sum2 = function sumNumbers(a, b) { // named function
+  return a + b;
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum2;
+console.log(sumAgain(1, 3));
+console.log('---------------');
+
+
+///// 2. Callback function using function expression /////
+//  Parameter로 전달 받은 함수를 Callback function이라고 한다
+function randomQuiz(answer, printYes, printNo) {
+  if (answer === 'love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+const printYes = function() { // anonymous function
+  console.log('yes!');
+};
+// Named function
+//  - better debugging in debugger's stack traces
+//  - recursions
+const printNo = function print() {  // named function
+  console.log('no!');
+  // print(); // recursions
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+console.log('---------------');
+
+
+///// 3. Arrow function /////
+//  - always anonymous function
+const simplePrint = function() {
+  console.log('simplePrint!');
+};
+const simplePrintArrow = () => console.log('simplePrint!!');
+
+const add = function(a, b) {
+  return a + b;
+}
+const addArrow = (a, b) => a + b;
+
+const simpleMuliply = (a, b) => {
+  return a * b;
+}
+console.log('---------------');
+
+
+///// 4. IIFE: Immediately Invoked Function Expression /////
+//  - function의 선언과 실행을 동시에 진행
+(function hello() {
+  console.log('IIFE');
+})();
