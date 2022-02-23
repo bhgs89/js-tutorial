487a2797e8fef7c1adf1c07e6759606a8ceec676
Baek Han <han.baek.program@gmail.com>
2022-02-22 21:16:09 -0800

    Add js array api - 2

--- array/array-api.js
+++ array/array-api.js
@@ -91,3 +91,83 @@ const students = [
   console.log(result);
   console.log('---------------');
 }
+
+///// Quiz 7: Make an array containing only the students' scores: result should be [45, 80, 90, 66, 88] /////
+{
+    // Array.map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[]
+    //  - Calls a defined callback function on each element of an array, and returns an array that contains the results
+    //  - Returns a copy of a section of an array.
+    //  - Do not change the original array
+    const result = students.map(student => student.score);
+    console.log(result);
+    console.log('---------------');
+}
+
+///// Quiz 8: Check if there is a student with the score lower than 50 /////
+{
+    // Array.some(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean
+    //  - Determines whether the specified callback function returns true for any element of an array.
+    const result = students.some(student => student.score < 50);
+    console.log(result);
+
+    // Array.every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean
+    //  - Determines whether all the members of an array satisfy the specified test.
+    const result2 = !students.every(student => student.score >= 50);
+    console.log(result2);
+    console.log('---------------');
+}
+
+///// Quiz 9: Compute students' average score /////
+{
+    // Array.reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
+    // Array.reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
+    //  - Calls the specified callback function for all the elements in an array. 
+    //  - The return value of the callback function is the accumulated result, and is provided as an argument in the next call to the callback function.
+    const result = students.reduce((prev, curr) => {
+        console.log('***********');
+        console.log(prev);
+        console.log(curr);
+        return prev + curr.score;
+    }, 0);
+    console.log(result / students.length);
+
+    // Array.reduceRight(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
+    // Array.reduceRight(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;    
+    //  - Calls the specified callback function for all the elements in an array, in descending order. 
+    //  - The return value of the callback function is the accumulated result, and is provided as an argument in the next call to the callback function.
+    const result2 = students.reduceRight((prev, curr) => {
+        console.log('***********');
+        console.log(prev);
+        console.log(curr);
+        return prev + curr.score;
+    }, 0);
+    console.log(result2 / students.length);
+    console.log('---------------');
+}
+
+///// Quiz 10: Make a string containing all the scores: result should be '45, 80, 90, 66, 88' /////
+{
+    const result = students
+        .map(student => student.score)
+        .join();
+    console.log(result);
+    console.log('---------------');
+}
+
+///// Quiz 11: Make a string containing all the scores in ascending order: result should be '45, 66, 80, 88, 90' /////
+{
+    // Array.sort(compareFn?: (a: T, b: T) => number): this
+    //  - Sorts an array in place.
+    //  - This method mutates the array and returns a reference to the same array.
+    //  - compareFn Function used to determine the order of the elements. 
+    //  - It is expected to return a negative value if first argument is less than second argument, zero 
+    //  - if they're equal and a positive value otherwise. 
+    //  - If omitted, the elements are sorted in ascending, ASCII character order.
+    const result = students
+        .map(student => student.score)
+        .sort((a, b) => a - b)  // Ascending
+        // .sort((a, b) => b - a)  // Descending
+        .join();
+    console.log(result);
+    console.log('---------------');
+}
